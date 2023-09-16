b4a4747f617a1105a787e109d6d4a284fa466e8a
Armaan Seth <seth.armaanseth@gmail.com>
2023-09-16 17:22:29 +0530

    initial-commit

---
+++ app.py
@@ -0,0 +1,38 @@
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from io import BytesIO
+
+model = tf.keras.models.load_model("model1 .h5")
+
+
+def predict(image: Image.Image):
+    image = image.resize((256, 256))
+    image = np.asfarray(image) / 255
+    image = np.expand_dims(image, 0)
+    result = np.argmax(model.predict(image), axis=1)
+    return result
+
+
+def read_imagefile(file) -> Image.Image:
+    image = Image.open(BytesIO(file))
+    return image
+
+app = FastAPI()
+
+@app.post("/predict")
+async def predict_api(file: UploadFile = File(...)):
+    print("t")
+    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    if not extension:
+        return "Image must be jpg or png format!"
+    image = read_imagefile(await file.read())
+    prediction = predict(image)[0]
+    print(type(prediction))
+    return {"id":int(prediction)}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app,host='0.0.0.0',port=8000,reload=True)
